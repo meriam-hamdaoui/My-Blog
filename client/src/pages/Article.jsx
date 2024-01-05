77dc4c916f40908ab8ca474911dfa249b66c39e7
meriam <meriam.hamdaoui@gmail.com>
2024-01-05 14:34:31 +0100

    article by name

--- client/src/pages/Article.jsx
+++ client/src/pages/Article.jsx
@@ -1,11 +1,30 @@
 import React from "react";
+import { useParams } from "react-router-dom";
+// content
+import { articles as articleContent } from "../content/article-content";
 
 const Article = () => {
+  const { name } = useParams();
+  // find the article by name
+  let article = articleContent.find((el) => el.name === name);
+
+  if (!article)
+    return (
+      <h1 className="sm:text-2xl text-lg font-bold my-6 text-amber-600 text-center">
+        Article does not exist
+      </h1>
+    );
+
   return (
     <div>
       <h1 className="sm:text-4xl text-2xl font-bold my-6 text-gray-900">
-        My Article
+        {article.title}
       </h1>
+      {article.content.map((el, index) => (
+        <p key={index} className="mx-auto leading-relaxed text-base mb-4">
+          {el}
+        </p>
+      ))}
     </div>
   );
 };
