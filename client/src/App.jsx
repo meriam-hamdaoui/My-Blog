71bbf4e393134e309a97d70f470071bcfc491bdd
meriam <meriam.hamdaoui@gmail.com>
2024-01-05 14:16:21 +0100

    navbar component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,20 +1,26 @@
+// packages
 import { Routes, Route } from "react-router-dom";
-
+// pages
 import Home from "./pages/Home";
 import About from "./pages/About";
 import ArticleList from "./pages/ArticleList";
 import Article from "./pages/Article";
+// components
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <div className="max-w-screen-md mx-auto pt-20">
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/articles-list" element={<ArticleList />} />
-        <Route path="/article" element={<Article />} />
-      </Routes>
-    </div>
+    <>
+      <Navbar />
+      <div className="max-w-screen-md mx-auto pt-20">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/articles-list" element={<ArticleList />} />
+          <Route path="/article" element={<Article />} />
+        </Routes>
+      </div>
+    </>
   );
 }
 
--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -4,8 +4,17 @@ const About = () => {
   return (
     <div>
       <h1 className="sm:text-4xl text-2xl font-bold my-6 text-gray-900">
-        About Me
+        About
       </h1>
+      <p className="mx-auto leading-relaxed text-base mb-4">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione, eaque.
+      </p>
+      <p className="mx-auto leading-relaxed text-base mb-4">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. At fugit sunt,
+        distinctio repudiandae sequi consequuntur fugiat deleniti quis odio
+        laboriosam in explicabo! At reprehenderit tempora minus numquam vel
+        quidem quod.
+      </p>
     </div>
   );
 };
