6262d12c2c441d7e66b3ca1bdd908a51de200e0f
meriam <meriam.hamdaoui@gmail.com>
2024-01-06 20:32:52 +0100

    add comment action

--- server/controllers/operationDB.js
+++ server/controllers/operationDB.js
@@ -12,7 +12,7 @@ exports.getByName = async (req, res) => {
   const articleName = req.params.name;
 
   // Using connectWithDB to perform operations with the database
-  connectWithDB(async (db) => {
+  await connectWithDB(async (db) => {
     // const articleList = ;
     const articleByName = await db
       .collection("articles")
@@ -25,3 +25,35 @@ exports.getByName = async (req, res) => {
     }
   }, res);
 };
+
+exports.addComment = async (req, res) => {
+  try {
+    let articleName = req.params.name;
+    let { author, text } = req.body;
+
+    await connectWithDB(async (db) => {
+      const articleByName = await db
+        .collection("articles")
+        .findOne({ name: articleName });
+
+      await db.collection("articles").updateOne(
+        { name: articleName },
+        {
+          $set: {
+            comments: [...articleByName.comments, { author, text }],
+          },
+        }
+      );
+
+      const updatedArticle = await db
+        .collection("articles")
+        .findOne({ name: articleName });
+
+      res.status(200).json(updatedArticle);
+    }, res);
+  } catch (error) {
+    res.status(400).json({ message: "ERROR updating article", error });
+  }
+};
+// Add a new Article in the DB
+exports.addArticle = async (req, res) => {};
--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const express = require("express");
 
 // get controllers
-const { getAll, getByName } = require("./controllers/operationDB");
+const { getAll, getByName, addComment } = require("./controllers/operationDB");
 
 // lunch express app
 const app = express();
@@ -18,35 +18,10 @@ const port = process.env.PORT || 8000;
 app.get("/api/articles", getAll);
 
 // get by name
-
 app.get("/api/articles/:name", getByName);
 
 // add comment
-// app.post("/api/articles/:name/add-comment", async (req, res) => {
-//   try {
-//     const articleName = req.params.name;
-//     const { author, text } = req.body;
-//     // connect db
-//     const client = await MongoClient.connect(process.env.DB_URI);
-//     const articles = await client.db("mernblog").collection("articles");
-
-//     // find and return all documents in the 'articles' collection
-//     const updateArticle = await articles.findOneAndUpdate({
-//       name: articleName,
-//       $comments: [...comments, { author, text }],
-//     });
-
-//     if (!updateArticle) {
-//       return res
-//         .status(404)
-//         .json({ message: "no article found with this name" });
-//     }
-
-//     res.json(updateArticle.comments);
-//   } catch (error) {
-//     return res.status(500).json(error.message);
-//   }
-// });
+app.post("/api/articles/:name/add-comment", addComment);
 
 // listen
 app.listen(port, () => console.log("server started at " + port));
