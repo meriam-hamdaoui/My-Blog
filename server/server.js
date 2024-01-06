29b0d7491c6e022da7675020669342c840a00d97
meriam <meriam.hamdaoui@gmail.com>
2024-01-06 18:05:34 +0100

    db connection established

--- server/server.js
+++ server/server.js
@@ -1,38 +1,102 @@
+require("dotenv").config();
 const express = require("express");
 
-// initiate app
+const { MongoClient } = require("mongodb");
+
+// PORT
+const port = process.env.PORT || 8000;
+
+// lunch express app
 const app = express();
-const PORT = 8000;
 
-const articlesInfo = {
-  "learn-react": {
-    comments: [],
-  },
-  "learn-node": {
-    comments: [],
-  },
-  "my-thoughts-on-learning-react": {
-    comments: [],
-  },
-};
+// data
+// const articles = [
+//   {
+//     name: "learn-react",
+//     comments: [{ author: "souad", text: "I like this articles" }],
+//   },
+//   {
+//     name: "learn-node",
+//     comments: [{ author: "mariem", text: "It needs some improvement" }],
+//   },
+//   {
+//     name: "my-thoughts-on-learning-react",
 
-// initialize middleware
-app.use(express.json({ extended: false }));
+//     comments: [],
+//   },
+// ];
 
-app.get("/api/articles/:name/comments", (req, res) => {
-  // get the article name from req.params
-  const articleName = req.params.name;
-  res.status(200).send(articlesInfo[articleName]);
+// middlewares
+app.use(require("cors")()); // allow cross-origin requests
+app.use(express.json({ extended: false })); // parse request body as JSON
+
+// get all
+app.get("/api/articles", async (req, res) => {
+  try {
+    // connect db
+    const client = await MongoClient.connect(process.env.DB_URI);
+    const articles = client.db("mernblog").collection("articles");
+
+    // find and return all documents in the 'articles' collection
+    const articleTab = await articles.find().toArray();
+
+    setTimeout(() => {
+      return res.status(200).json(articleTab);
+    }, 3000);
+  } catch (error) {
+    return res.status(500).json(error.message);
+  }
 });
 
-// text api's
-app.post("/api/articles/:name/add-comments", (req, res) => {
-  const { username, text } = req.body;
-  const articleName = req.params.name;
-  articlesInfo[articleName].comments.push({ username, text });
-  res.status(200).send(articlesInfo[articleName]);
+// get by name
+app.get("/api/articles/:name", async (req, res) => {
+  try {
+    const articleName = req.params.name;
+    // connect db
+    const client = await MongoClient.connect(process.env.DB_URI);
+    const db = client.db("mernblog");
+    const articleList = db.collection("articles");
+
+    // find and return all documents in the 'articles' collection
+    const articleByName = await articleList.findOne({ name: articleName });
+
+    if (!articleByName) res.status(404).json(articleByName);
+
+    if (articleByName)
+      setTimeout(() => res.status(200).json(articleByName), 2000);
+
+    client.close();
+  } catch (error) {
+    res.status(500).json(error.message);
+  }
 });
 
-// port
+// add comment
+app.post("/api/articles/:name/add-comment", async (req, res) => {
+  try {
+    const articleName = req.params.name;
+    const { author, text } = req.body;
+    // connect db
+    const client = await MongoClient.connect(process.env.DB_URI);
+    const articles = await client.db("mernblog").collection("articles");
 
-app.listen(PORT, () => console.log("server started at " + PORT));
+    // find and return all documents in the 'articles' collection
+    const updateArticle = await articles.findOneAndUpdate({
+      name: articleName,
+      $comments: [...comments, { author, text }],
+    });
+
+    if (!updateArticle) {
+      return res
+        .status(404)
+        .json({ message: "no article found with this name" });
+    }
+
+    res.json(updateArticle.comments);
+  } catch (error) {
+    return res.status(500).json(error.message);
+  }
+});
+
+// listen
+app.listen(port, () => console.log("server started at " + port));
